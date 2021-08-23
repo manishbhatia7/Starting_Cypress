4e85329aade52f6044b569d12b5a58030049bea7
mbhatia-ML-datascience <74395142+mbhatia-ML-datascience@users.noreply.github.com>
2021-08-23 18:47:07 +0530

    Commit2

--- cypress/integration/1-getting-started/file_upload.js
+++ cypress/integration/1-getting-started/file_upload.js
@@ -1,14 +1,19 @@
 describe('Different scenarios of file upload', () => {
-    it('Single File Upload-DOM', () => {
+    it.skip('Single File Upload-DOM', () => {
         cy.visit('http://127.0.0.1:5500/Help%20Folder/fileupload.html')
         cy.get('#file-upload1').attachFile('dog_small.jpg')
         cy.get('span#fileName1').should('have.text','dog_small.jpg')
     });
-    it('', () => {
-        
+    it.skip('Single File Upload-DOM-Different Name', () => {
+        cy.visit('http://127.0.0.1:5500/Help%20Folder/fileupload.html')
+        cy.get('#file-upload1').attachFile({filePath:'dog_small.jpg',fileName:'puppy.jpg'})
+        cy.get('span#fileName1').should('have.text','puppy.jpg')
     })
-    it('', () => {
-        
+    it.only('Attaching Multiple files', () => {
+        cy.visit('http://127.0.0.1:5500/Help%20Folder/fileupload.html')
+        cy.get('#file-upload2')
+        .attachFile('example.json')
+        .attachFile('dog_small.jpg')
     })
     
 });
